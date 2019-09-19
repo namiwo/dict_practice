b203baed8066d0a7e1d63ed68f4cf639189ca879
namiwo <nhsantasan@gmail.com>
2019-09-19 15:10:31 +0900

    辞書の機能の確認

---
+++ fruits.py
@@ -0,0 +1,17 @@
+def main():
+    fruits_dict = {
+        "りんご": "Apple",
+        "ぶどう": "Grape",
+        "みかん": "Orange"
+    }
+
+    # Grapeと出力するコードを書く
+    print(fruits_dict["ぶどう"])
+    # Orangeと出力するコードを書く
+    print(fruits_dict["みかん"])
+    # Appleと出力するコードを書く
+    print(fruits_dict["りんご"])
+
+
+if __name__ == '__main__':
+    main()
